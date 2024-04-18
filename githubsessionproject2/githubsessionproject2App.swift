4241b0aca2fcf73413b583e70b504ee783703ae2
Kim <minjg4455@naver.com>
2024-04-18 20:15:26 +0900

    Initial Commit

---
+++ githubsessionproject2/githubsessionproject2App.swift
@@ -0,0 +1,17 @@
+//
+//  githubsessionproject2App.swift
+//  githubsessionproject2
+//
+//  Created by 김민정 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct githubsessionproject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ githubsessionproject2UITests/githubsessionproject2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  githubsessionproject2UITestsLaunchTests.swift
+//  githubsessionproject2UITests
+//
+//  Created by 김민정 on 4/18/24.
+//
+
+import XCTest
+
+final class githubsessionproject2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
